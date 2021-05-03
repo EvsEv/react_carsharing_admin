445b099f12511850b045f0940ae6aaba62ae6dab
EvsEv <enevstigneev@gmail.com>
2021-05-03 19:57:51 +0400

    v07.layoutAuth created base structure for login layout

---
+++ src/layouts/LoginLayout/LoginLayout.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import styles from "./loginLayout.module.sass";
+
+export const LoginLayout = () => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.logo}>Logo</div>
+            <form className={styles.form}>
+                <h1 className={styles.title}>Вход</h1>
+                <div className={styles.field}>
+                    <label htmlFor="mail" className={styles.label}>
+                        Почта
+                    </label>
+                    <input
+                        className={styles.input}
+                        type="mail"
+                        id="mail"
+                        required
+                        placeholder="Введите почту"
+                    />
+                </div>
+                <div className={styles.field}>
+                    <label htmlFor="password" className={styles.label}>
+                        Пароль
+                    </label>
+                    <input
+                        className={styles.input}
+                        type="password"
+                        id="password"
+                        required
+                        placeholder="Введите пароль"
+                    />
+                </div>
+                <div className={styles.access}>
+                    <a href="#" className={styles.request}>
+                        Запросить доступ
+                    </a>
+                    <button type="submit"> Войти</button>
+                </div>
+            </form>
+        </div>
+    );
+};
---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,11 @@
+const initialState = {
+    login: null,
+    isAuth: false,
+};
+
+export const auth = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
