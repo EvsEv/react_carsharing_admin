2309f044b08b842031f8ddcfd6ddf6fec54f5501
EvsEv <enevstigneev@gmail.com>
2021-05-03 22:59:24 +0400

    v07-f08.auth added hashRouter to project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { HashRouter } from "react-router-dom";
 import AdminLayout from "../../layouts/AdminLayout";
 import LoginLayout from "../../layouts/LoginLayout";
 
 export const App = () => {
     const { isAuth } = useSelector((state) => state.auth);
 
-    if (!isAuth) {
-        return <LoginLayout />;
-    }
-
-    return <AdminLayout />;
+    return (
+        <HashRouter basename="/">
+            {isAuth ? <AdminLayout /> : <LoginLayout />}
+        </HashRouter>
+    );
 };
