4949422898681bd8132bfa9e3215fc26ed2b075b
EvsEv <enevstigneev@gmail.com>
2021-05-04 21:45:26 +0400

    v08.layoutOrderList created footer for desktop only

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styles from "./footer.module.sass";
 
 export const Footer = () => {
-    return <footer className={styles.footer}>Footer test</footer>;
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.linksBlock}>
+                <Link to="/" className={styles.link}>
+                    Главная страница
+                </Link>
+                <a
+                    target="_blank"
+                    href="https://evsev.github.io/react_carsharing/build/#/"
+                    className={styles.link}
+                >
+                    Клиентская версия
+                </a>
+            </div>
+            <p className={styles.copyright}>Copyright © 2020 Simbirsoft</p>
+        </footer>
+    );
 };
