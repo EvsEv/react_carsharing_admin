23369d9f3138d1eeb2678162f6345a21beb60319
EvsEv <enevstigneev@gmail.com>
2021-05-24 05:00:04 +0400

    v08.layoutOrderList adapted for 1024

--- src/components/UIKit/Paginate/Paginate.js
+++ src/components/UIKit/Paginate/Paginate.js
@@ -50,14 +50,13 @@ export const Paginate = ({ activePage, countOfPages, changePage }) => {
 
     const onClick = (page) => {
         if (page === "...") return setShowSearchDropdown(true);
-        if (page === "»") return dispatch(changePage(activePage + 1));
         dispatch(changePage(page));
     };
 
     const decreasePage = () => dispatch(changePage(activePage - 1));
     const increasePage = () => dispatch(changePage(activePage + 1));
 
-    const onTypingInsearch = (event) => {
+    const onTypingInSearch = (event) => {
         event.target.validity.valid && setSearchingPage(event.target.value);
     };
 
@@ -108,7 +107,7 @@ export const Paginate = ({ activePage, countOfPages, changePage }) => {
                         id="searchPage"
                         className={styles.search}
                         value={searchingPage}
-                        onChange={onTypingInsearch}
+                        onChange={onTypingInSearch}
                         type="number"
                         min="0"
                         max={countOfPages + 1}
